7ab9c731a9bd15d1dd9414863c621597c6f8d76b
samuelgruetter <samuel.gruetter@epfl.ch>
2013-12-10 13:08:09 +0100

    modify build config

--- project/SourcePrinter.scala
+++ project/SourcePrinter.scala
@@ -3,14 +3,10 @@ import Keys._
 import Configurations.CompilerPlugin
 import sbtassembly.Plugin._
 import AssemblyKeys._
-import com.typesafe.sbt.SbtSite.site
-import com.typesafe.sbt.SbtSite.SiteKeys._
-import com.typesafe.sbt.site.JekyllSupport.Jekyll
-import com.typesafe.sbt.SbtGhPages.ghpages
-import com.typesafe.sbt.SbtGit.git
 
 object SourcePrinter extends Build {
-  val printPluginName = "printplugin"
+  val myPluginName = "srewriteplugin"
+  
   val jarsToExclude = Seq("scala-reflect.jar", "scala-library.jar", "scala-compiler.jar")
   val buildSettings = Defaults.defaultSettings ++
     Seq(
@@ -24,26 +20,15 @@ object SourcePrinter extends Build {
       }
     ) 
 
-    val printPlugin = Project(printPluginName, file("."),
-      settings = buildSettings ++ assemblySettings ++ assemblyProjectSettings ++ websiteSettings ++
-      addArtifact(Artifact(printPluginName, "assembly"), sbtassembly.Plugin.AssemblyKeys.assembly)) settings (
-	     name := printPluginName,
+    val srewriteplugin = Project(myPluginName, file("."),
+      settings = buildSettings ++ assemblySettings ++ assemblyProjectSettings ++
+      addArtifact(Artifact(myPluginName, "assembly"), sbtassembly.Plugin.AssemblyKeys.assembly)) settings (
+         name := myPluginName,
 
       //crossVersion := CrossVersion.full,
       //exportJars := true,
       libraryDependencies <++= scalaVersion apply dependencies
     ) 
-
-    lazy val websiteSettings: Seq[Setting[_]] = (
-     site.settings ++
-     ghpages.settings ++
-     site.includeScaladoc() ++
-     site.jekyllSupport() ++
-     Seq(
-       git.remoteRepo := "https://github.com/VladimirNik/printPlugin.git",
-       includeFilter in Jekyll := ("*.html" | "*.png" | "*.js" | "*.css" | "CNAME")
-     )
-   )
 
     def dependencies(sv: String) = Seq(
       "org.scala-lang" % "scala-compiler" % sv,
