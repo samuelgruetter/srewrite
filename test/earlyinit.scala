e50366beb23b53ca7344dec6e7bc804c9d56f39d
Samuel Gruetter <samuel.gruetter@epfl.ch>
2014-11-27 13:33:26 +0100

    extend earlyinit example (cannot just comment out early initializations!)

--- test/earlyinit.scala
+++ test/earlyinit.scala
@@ -6,11 +6,26 @@ object test {
    trait Bar {
       val y: Int
    }
-   def foo: Foo = new { val x: Int = 12345; val z: Int = 54321 } with Foo with Bar {
+   def foo: Foo = new { val x: Int = 12345; val y: Int = 54321 } with Foo with Bar {
      val normalInit: Int = 333
-   }
+   } 
+   def foo2: Foo = new { val x: Int = 12345; val y: Int = 54321 } with Foo with Bar
    def bar: Bar = {
      println
      new { val y = 7777 } with Bar { val yy = 8888 }
    }
+   
+   final class DupIterator[T](xs: T) extends {
+      val x = 22222
+      val init2 = 333333
+   } with Foo {
+     final def getElem(x: AnyRef): T = ???
+   }
+
+   final class DupIterator2[T](xs: T) extends {
+      val x = 22222
+      val init2 = 333333
+   } with Foo
+   
+   lazy val a: DupIterator[Int] = ???
 }
